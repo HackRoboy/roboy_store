0ca4d24e765a83407d40e6d4f9c5b0e1d9d48356
Mistu <mttmistura@gmail.com>
2017-04-30 17:47:27 +0200

    update

--- src/store.py
+++ src/store.py
@@ -3,18 +3,69 @@
 import rospy
 import json
 import subprocess
+import os
+import shutil
 from std_msgs.msg import String
 
+def getPaths():
+    parser = os.path.dirname(os.path.abspath(__file__)).split("/")
+    str=""
+    path_src = ""
+    path_store = ""
+    path_catkin_ws = ""
+    parser_len = len(parser)
+    for i in range(parser_len):
+	str = str + parser[i] +"/"
+	if (i==parser_len-4):
+	    path_catkin_ws = str
+	elif(i==parser_len-3):
+	    path_src = str
+        elif(i==parser_len-2):
+            path_store= str
+    print("path_src:"+path_src)
+    print("path_store:"+path_store)
+    print("path_catkin_ws:"+path_catkin_ws)
+ #   print("path: "+os.path.dirname(os.path.abspath(__file__)))
+	
+	
+    return [path_src, path_store, path_catkin_ws]
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + 'Installing %s...', data.data)
     
-    with open('/home/dennj/catkin_ws/src/roboy_store/app_list.json') as data_file:    
+    path = getPaths()
+    path_src = path[0]
+    path_store = path[1]
+    path_catkin_ws = path[2]
+
+
+    with open(path_store+'app_list.json') as data_file:    
         apps = json.load(data_file)
 
     pkg = data.data
-    cmd = "git clone '"+apps[pkg]+"' '/home/dennj/catkin_ws/src/"+pkg+"'"
-    print cmd
+    cmd = "git clone "+apps[pkg]+" "+path_src+"src/"+pkg
     output = subprocess.check_output(['bash','-c', cmd])
+
+    print "Done"
+
+    cmd = "roslaunch "+pkg+" "+"*.launcher"
+
+def removecall(data):
+    rospy.loginfo(rospy.get_caller_id() + 'Removing %s...', data.data)
+	
+    path = getPaths()
+    path_src = path[0]
+    path_store = path[1]
+    path_catkin_ws = path[2]
+
+    with open(path_store+'app_list.json') as data_file:    
+      	apps = json.load(data_file)
+	
+    pkg = data.data
+    #cmd = "rmdir " + path_src + pkg + ' -r';
+
+    #output = subprocess.check_output(['bash','-c', cmd])
+    shutil.rmtree(path_src + pkg)
+	
 
 def store():
 
@@ -24,7 +75,8 @@ def store():
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('store', anonymous=True)
-    rospy.Subscriber('telegram', String, callback)
+    rospy.Subscriber('telegram_install', String, callback)
+    rospy.Subscriber('telegram_remove', String, removecall)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
